65e038cc0ed0b7baf5462bf877062764d9d835eb
Timo Weiß <t.weiss@mwaysolutions.com>
2017-09-12 22:13:40 +0200

    add comments

--- renderer/src/components/time.jsx
+++ renderer/src/components/time.jsx
@@ -6,12 +6,14 @@ import ReactTooltip from 'react-tooltip';
 import { getDetailedTimeFromTo, getDetailedTimeToNow, ta } from './../helper/zeit';
 
 
+// shows something like 'started 20 minutes ago' or, if finished 20 minutes
 export const TimeContent = ({ item }) => (
   <div data-tip data-for={`${item.k}tt`}>
     <span>{item.finished ? ta.duration(item.created, item.finished) : `started ${ta.ago(item.created)}`}</span>
   </div>
 );
 
+// adds an 's' to var singular if num is bigger than one
 export const PluralSingular = ({ num, singular }) => {
   if (num !== undefined) {
     return (<span>{num} {num > 1 ? `${singular}s` : singular} </span>);
@@ -20,6 +22,7 @@ export const PluralSingular = ({ num, singular }) => {
 };
 
 
+// renders time, wrapped with detailed time as tooltip
 export const Time = ({ item }) => (
   <div style={{ fontSize: '10px' }}>
     <ReactTooltip
@@ -33,6 +36,8 @@ export const Time = ({ item }) => (
   </div>
 );
 
+
+// renders time on second precision
 export const DetailedTime = ({ from, to }) => {
   const dt = to ? getDetailedTimeFromTo(from, to) : getDetailedTimeToNow(from);
   return (
