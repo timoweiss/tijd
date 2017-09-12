103fb4da5cb668c56ffca231c89c6fd7b9d311cd
Timo Weiß <t.weiss@mwaysolutions.com>
2017-09-12 22:27:45 +0200

    add emoji translation for hi, bye and break

--- renderer/src/components/pastItem.jsx
+++ renderer/src/components/pastItem.jsx
@@ -5,9 +5,20 @@ import InputRange from 'react-input-range';
 
 import { Time } from './time';
 
+const typeEmojiMap = {
+  break: '☕',
+  hi: 'hi 👋',
+  bye: '👋 bye',
+};
+
 const PastItem = ({ item, showEdit }) => (
-  <div className="past-item">
-    <span>{item.name}</span>
+  <div
+    className="past-item"
+    style={{
+      background: item.type ? 'white' : null,
+    }}
+  >
+    <span>{typeEmojiMap[item.type] ? typeEmojiMap[item.type] : item.name}</span>
     <Time item={item} />
     {showEdit && <div style={{ width: '100%' }}>
       <hr />
