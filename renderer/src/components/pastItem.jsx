f8e746778614b33db8832e933c7e30e337850949
Timo Weiß <t.weiss@mwaysolutions.com>
2017-09-21 22:15:36 +0200

    init EditItem

--- renderer/src/components/pastItem.jsx
+++ renderer/src/components/pastItem.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 import InputRange from 'react-input-range';
 
+import 'react-input-range/lib/css/index.css';
+
 import { Time } from './time';
 
 const typeEmojiMap = {
@@ -11,19 +13,34 @@ const typeEmojiMap = {
   bye: '👋 bye',
 };
 
+class EditItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      min: props.value.min,
+      max: props.value.max,
+    };
+  }
+  render() {
+    return (
+      <div style={{ width: '100%' }}>
+        <hr />
+        <InputRange
+          maxValue={this.props.maxValue || 0}
+          minValue={this.props.minValue || 100}
+          value={{ min: 2, max: 10 }}
+          onChange={value => this.setState({ value })}
+        />
+        edit view</div>
+    );
+  }
+}
+
 const PastItem = ({ item, showEdit }) => (
   <div className="past-item">
     <span>{typeEmojiMap[item.type] ? typeEmojiMap[item.type] : item.name}</span>
     <Time item={item} />
-    {showEdit && <div style={{ width: '100%' }}>
-      <hr />
-      <InputRange
-        maxValue={20}
-        minValue={0}
-        value={{ min: 2, max: 10 }}
-        onChange={value => this.setState({ value })}
-      />
-      edit view</div>}
+    {showEdit && <EditItem value={{ min: 2, max: 10 }} />}
   </div>
 );
 
