ba24d04458e8543bcc9d720d8af35ced94772b91
michaelknoch <info@michaelknoch.de>
2017-09-26 21:39:00 +0200

    wip implementation of auto update

--- README.md
+++ README.md
@@ -27,3 +27,11 @@ npm start
 # ./
 npm run dev
 ```
+
+### Deploy to Github release artifacts
+´´´
+npm run deploy
+```
+Builds and deploys a [release artifact](https://github.com/timoweiss/tijd/releases).
+Electron-autoUpdater will fetch the newest release automatically when starting the app. 
+Ensure you set an env variable [GH_TOKEN](https://github.com/settings/tokens) with a personal access token of a user with write access for this repo.
---
+++ main/update.js
@@ -0,0 +1,25 @@
+const req = require('request');
+const semver = require('semver');
+const { version: currentVersion } = require('../package.json');
+
+const getLatestDownloadPath = function() {
+    const options = {
+        url: 'https://api.github.com/repositories/101575843/releases',
+        headers: { 'User-Agent': 'michaelknoch' }
+    };
+
+    req(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const { assets } = JSON.parse(body)[0];
+            const latestRelease = assets.filter(
+                asset => asset.content_type == 'application/zip'
+            )[0];
+
+            return latestRelease.browser_download_url;
+        }
+    });
+}
+
+module.exports = {
+    getLatestDownloadPath
+};
