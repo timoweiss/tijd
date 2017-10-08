45ca0429cfd87f9a63c3a234c0151e618c42b1b4
michaelknoch <info@michaelknoch.de>
2017-10-08 14:17:17 +0200

    refactor

--- main/update.js
+++ main/update.js
@@ -1,45 +1,20 @@
-const { autoUpdater } = require('electron'); // eslint-disable-line
-const req = require('request');
-const semver = require('semver');
+const GhReleases = require('electron-gh-releases');
 const { version: currentVersion } = require('../package.json');
 
-function getLatestDownloadPath() {
-  const options = {
-    url: 'https://api.github.com/repositories/101575843/releases',
-    headers: { 'User-Agent': 'michaelknoch' },
-  };
-
-  return new Promise(((resolve, reject) => {
-    req(options, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        const { assets, tag_name } = JSON.parse(body)[0];
-        const latestZip = assets.find(asset => asset.content_type === 'application/zip');
-
-        /* eslint-disable no-console */
-        console.log('currently on', currentVersion);
-        console.log('found version', semver.clean(tag_name));
-        /* eslint-enable no-console */
-
-        if (semver.lt(currentVersion, semver.clean(tag_name))) {
-          resolve(latestZip.browser_download_url);
-        }
-        reject(`you are up to date with ${currentVersion}`);
-      }
-      reject('error during update', error);
-    });
-  }));
-}
+const updater = new GhReleases({
+  repo: 'timoweiss/tijd',
+  currentVersion,
+});
 
 function checkForUpdates() {
-  getLatestDownloadPath()
-    .then((feedUrl) => {
-      autoUpdater.setFeedURL(feedUrl);
-      autoUpdater.checkForUpdates();
-    })
-    .catch(e => console.log(e));
+  updater.check((err, status) => {
+    if (!err && status) {
+      updater.download();
+    }
+  });
 
-  autoUpdater.on('update-downloaded', () => {
-    autoUpdater.quitAndInstall();
+  updater.on('update-downloaded', () => {
+    updater.install();
   });
 }
 
